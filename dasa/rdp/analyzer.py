622bae3bf0ef6e15a8da1a024e2091b6d2da9ecd
Wladimir Sidorenko <wlsidorenko@gmail.com>
2018-11-13 22:03:56 +0100

    added compatibility support for parameters `gird_search` and `balance` to the `_train` method

--- dasa/rdp/analyzer.py
+++ dasa/rdp/analyzer.py
@@ -84,7 +84,7 @@ class RDPAnalyzer(R2N2Analyzer):
         super(RDPAnalyzer, self)._init_wbenches()
         self._wbench_y = np.zeros(1, dtype="long")
 
-    def _train(self, train_set, dev_set):
+    def _train(self, train_set, dev_set, grid_search=True, balance=False):
         """Train specified model(s) on the provided data.
 
         Args:
