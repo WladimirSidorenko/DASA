3e0d83cfcdb1804ba9fdec8f0ac87d9754dbe6a9
Wladimir Sidorenko <wlsidorenko@gmail.com>
2020-09-08 07:56:09 +0200

    upd simplified RDM file

--- 1.py
+++ 1.py
@@ -10,6 +10,7 @@ from pyro.distributions import (
 )
 from torch.nn import Softmax
 import numpy as np
+import pyro
 
 
 ##################################################################
@@ -38,8 +39,32 @@ class AlphaModel(PyroModule):
     def M(self):
         return MultivariateNormal(self.M_Mu, self.M_Sigma)
 
+    def _get_child_scores(self, node_scores, children, inst_indices, i,
+                          n_instances, max_children):
+        child_indices = children[inst_indices, i].reshape(-1)
+        inst_indices.repeat(max_children, 1).t().reshape(-1)
+        child_scores = node_scores[
+            inst_indices.repeat(max_children, 1).t().reshape(-1),
+            child_indices
+        ].reshape(n_instances, max_children, -1)
+        return child_scores
+
     def forward(self, node_scores, children, rels, labels):
         n_instances = node_scores.shape[0]
+        max_depth = node_scores.shape[1]
+        max_width = children.shape[-1]
+        with pyro.plate("batch", size=n_instances) as inst_indices:
+            print("inst_indices", inst_indices)
+            for i in range(max_depth):
+                print("i:", i)
+                prnt_scores_i = node_scores[inst_indices, i]
+                print("prnt_scores_i:", prnt_scores_i)
+                rels_i = rels[inst_indices, i]
+                print("rels_i:", rels_i)
+                child_scores_i = self._get_child_scores(
+                    node_scores, children, inst_indices, i,
+                    n_instances, max_width
+                )
         return self.M
 
 
@@ -63,4 +88,4 @@ print("rels", rels)
 
 labels = np.array([0, 1], dtype=np.int32)
 print("labels", rels)
-# print(a())
+print("a():", a(node_scores, children, rels, labels))
